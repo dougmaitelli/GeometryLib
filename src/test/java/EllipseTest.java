824ee28454abe7487463404d3f083f566f194c4d
Douglas Maitelli <dougmaitelli@gmail.com>
2017-08-15 19:16:36 +0200

    Added Ellipse Tests

--- src/test/java/EllipseTest.java
+++ src/test/java/EllipseTest.java
@@ -1,8 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import Geometry.Circle;
 import Geometry.Ellipse;
 import Geometry.Line;
+import Geometry.LineSegment;
 import Geometry.Point;
 import Geometry.Position;
 
@@ -20,12 +22,85 @@ public class EllipseTest {
 		Assert.assertEquals(47.1238, ellipse.getArea(), 0.0001);
 	}
 	
+	
 	@Test
-	public void testLinePosition() {
+	public void testLinePositionInside() {
 		Ellipse ellipse = getTestEllipse();
 		
-		Line s = new Line(0.5, 0);
+		Line s = new Line(1, 0);
 		
-		Assert.assertEquals(Position.INSIDE, ellipse.getLinePosition(s));
+		Assert.assertTrue(ellipse.isLineInside(s));
+	}
+	
+	@Test
+	public void testLinePositionOutside() {
+		Ellipse ellipse = getTestEllipse();
+		
+		Line s = new Line(1, -10);
+		
+		Assert.assertTrue(ellipse.isLineOutside(s));
+	}
+	
+	@Test
+	public void testLinePositionBelong() {
+		Ellipse ellipse = getTestEllipse();
+		
+		Line s = new Line(0, 2);
+		
+		Assert.assertTrue(ellipse.isLineBelong(s));
+	}
+	
+	@Test
+	public void testLineSegmentPositionInside() {
+		Ellipse ellipse = getTestEllipse();
+		
+		LineSegment s = new LineSegment(new Point(0, 4), new Point(6, 4));
+		
+		Assert.assertTrue(ellipse.isLineInside(s));
+	}
+	
+	@Test
+	public void testLineSegmentPositionOutside() {
+		Ellipse ellipse = getTestEllipse();
+		
+		LineSegment s = new LineSegment(new Point(6, 2), new Point(10, 2));
+		
+		Assert.assertTrue(ellipse.isLineOutside(s));
+	}
+	
+	@Test
+	public void testLineSegmentPositionBelong() {
+		Ellipse ellipse = getTestEllipse();
+		
+		LineSegment s = new LineSegment(new Point(0, 10), new Point(6, 10));
+		
+		Assert.assertTrue(ellipse.isLineBelong(s));
+	}
+	
+	@Test
+	public void testPointPositionInside() {
+		Ellipse ellipse = getTestEllipse();
+		
+		Point p = new Point(2, 4);
+		
+		Assert.assertTrue(ellipse.isPointInside(p));
+	}
+	
+	@Test
+	public void testPointPositionOutside() {
+		Ellipse ellipse = getTestEllipse();
+		
+		Point p = new Point(2, 10);
+		
+		Assert.assertTrue(ellipse.isPointOutside(p));
+	}
+	
+	@Test
+	public void testPointPositionBelong() {
+		Ellipse ellipse = getTestEllipse();
+		
+		Point p = new Point(6, 5);
+		
+		Assert.assertTrue(ellipse.isPointBelong(p));
 	}
 }
